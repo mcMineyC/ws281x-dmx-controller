3a01008af546e51e68f8fc4fbcbb1b65a11f93d4
mcMineyC <jedi.master@falconhosting.net>
2025-10-09 19:16:45 -0700

    add sketch, needs modification

---
+++ dmx_ws2812_receiver/dmx_ws2812_receiver.ino
@@ -0,0 +1,65 @@
+// DMX Receiver/Fixture Example for WS2812B RGB LEDs
+// Target Hardware:  Arduino Uno
+//
+// Receives individual RGB levels for 60 WS2812B LEDs (60*3 = 180 DMX channels total)
+// over an RS485 link in DMX packet format, to be decoded and sent out to a 60 LED strip.
+// The test pattern is an LED chaser where two different color bars run end to end
+// along the strip at different speeds, crossing over each other when they meet.
+//
+// The first DMX channel is 1 so the 60 LED RGB values are sent sequentially in
+// channels 1 to 180, eg:
+// Ch   1: LED  0 RED   value [0-255]
+// Ch   2: LED  0 GREEN value [0-255]
+// Ch   3: LED  0 BLUE  value [0-255]
+// Ch 178: LED 59 RED   value [0-255]
+// Ch 179: LED 59 GREEN value [0-255]
+// Ch 180: LED 59 BLUE  value [0-255]
+//
+// Required library:
+//    DMXSerial        install from library manager or https://github.com/mathertel/DMXSerial
+//
+// Gadget Reboot
+// https://www.youtube.com/gadgetreboot
+
+#include <DMXSerial.h>
+#include "ws2812.h"                // a specific LED controller that disables interrupts to work better
+
+#define NUM_LEDS 48                // number of RGB LEDs on strip
+#define DMXSTART 24*3 + 1                 // first DMX channel
+#define DMXLENGTH (NUM_LEDS*3)+(DMXSTART-1)     // number of DMX channels used (3*48 LEDs)
+
+byte blankRgbData[DMXLENGTH];
+
+void setup () {
+  for(int i = 0; i < NUM_LEDS; i++){
+    if(i < NUM_LEDS/3){
+      blankRgbData[i*3] = 255;
+      blankRgbData[i*3+1] = 0;
+      blankRgbData[i*3+2] = 0;
+    }else if(i < (NUM_LEDS/3)*2){
+      blankRgbData[i*3] = 0;
+      blankRgbData[i*3+1] = 255;
+      blankRgbData[i*3+2] = 0;
+    }else{
+      blankRgbData[i*3] = 0;
+      blankRgbData[i*3+1] = 0;
+      blankRgbDatai*3+2 = 255;
+    }
+  }
+
+  DMXSerial.init(DMXProbe);        // initialize DMX bus in manual access mode
+  DMXSerial.maxChannel(DMXLENGTH); // "onUpdate" will be called when all new ch data has arrived
+
+  setupNeopixel();                 // setup the LED output hardcoded to pin 12 in ws2812.h
+  updateNeopixel(blankRgbData, NUM_LEDS);
+  delay(1000);
+}
+
+
+void loop() {
+  // wait for an incomming DMX packet and write
+  // the RGB data for 60 LEDs on the strip
+  if (DMXSerial.receive()) {
+    updateNeopixel(DMXSerial.getBuffer() + DMXSTART, NUM_LEDS);
+  }
+}
