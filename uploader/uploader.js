4ae162685697ea732a269f0bce4868d5f498c455
mcMineyC <jedi.master@falconhosting.net>
2025-10-10 10:31:46 -0700

    add testing button

--- uploader/uploader.js
+++ uploader/uploader.js
@@ -4,6 +4,17 @@ let avrgirl = new AvrgirlArduino({
   debug: true,
   timeout: 1000,
 });
+var testbin = null;
+async function fetchTest() {
+  if (testbin !== null) return testbin;
+
+  var url = `https://raw.githubusercontent.com/noopkat/avrgirl-arduino/refs/heads/master/junk/hex/uno/Blink.cpp.hex`;
+  const response = await axios.get("https://proxy.corsfix.com/?" + url, {
+    responseType: "arraybuffer", // Request the response as an ArrayBuffer
+  });
+  testbin = response.data;
+  return response.data;
+}
 async function fetchBinary(dmxstart, pixeltype, numled) {
   if (
     lastFlash.dmxstart == dmxstart &&
@@ -23,6 +34,30 @@ async function fetchBinary(dmxstart, pixeltype, numled) {
     buffer: response.data,
   };
   return response.data;
+}
+async function testClicked() {
+  console.log("test clicked");
+  var binary = await fetchTest();
+  try {
+    avrgirl.flash(binary, (error) => {
+      if (error) {
+        window.showAvrgirlError(error.toString().substring(0, 42));
+        // setTimeout(() => window.hideAvrgirlError(), 1000);
+        console.log(error);
+      } else {
+        window.hideAvrgirlError();
+        console.info("done correctly.");
+      }
+    });
+  } catch (e) {
+    if (e.toString().includes("requestPort")) {
+      alert(
+        "Please use Chrome.\nThis tool needs WebUSB which for some reason\nis only available in Chrome-based browsers :/",
+      );
+    } else {
+      console.error(e);
+    }
+  }
 }
 async function flashClicked() {
   console.log("Flash clicked");
